83a6544e7c57574708f353d9d41901e145d2ddff
Pablo Zarate <pablo@nucleuscare.com>
2025-03-14 17:34:36 -0600

    feat: implement components in dashboard page

---
+++ src/app/components/FilesTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilesTable = () => {
+    return (
+        <div>FilesTable</div>
+    )
+}
+
+export default FilesTable
--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import WrapperContainer from "@/app/components/WrapperContainer";
+import ContentContainer from "@/app/components/ContentContainer";
+import Sidebar from "@components/Sidebar";
+import Topbar from "@components/Topbar";
+import StatsCard from "@components/StatsCard";
+import TrendsChart from "@components/TrendsChart";
+import FilesTable from "@components/FilesTable";
+import PageTopBar from "@/app/components/PageTopBar";
 
 const DashboardPage = () => {
     return (
-        <div>DashboardPage</div>
-    )
-}
+        <WrapperContainer>
+            <div className="hidden sm:block w-64">
+                <Sidebar />
+            </div>
+            <div className="block sm:hidden">
+                <Topbar />
+            </div>
+            <ContentContainer>
+                <PageTopBar pageTitle="Overview" />
+                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-x-6 gap-y-4">
+                    <StatsCard title="Unresolved" value="60" />
+                    <StatsCard title="Overdue" value="16" />
+                    <StatsCard title="Open" value="43" />
+                    <StatsCard title="On hold" value="64" />
+                </div>
+                <div className="mt-6">
+                    <TrendsChart />
+                </div>
+                <div className="mt-6">
+                    <FilesTable />
+                </div>
+            </ContentContainer>
+        </WrapperContainer>
+    );
+};
 
-export default DashboardPage
+export default DashboardPage;
